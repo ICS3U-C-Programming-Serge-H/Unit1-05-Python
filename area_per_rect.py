f908c410b4107850c60135ecbbae964ed6cabf1f
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-02-20 17:24:09 +0000

    commit area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Serge Hamouche
+# Date:Feb 18, 20
+# This program calculates the area & perimeter of a 5cm X 3cm rectangle.
+
+
+def main():
+    print("For a rectangle with length 5cm and width 3cm:")
+    print()
+    print ("The area is: {}cm^2".format(7 * 5))
+    print ("The perimeter is: {}cm".format(2 * (7 + 5)))
+
+
+if __name__ == "__main__":
+    main()
